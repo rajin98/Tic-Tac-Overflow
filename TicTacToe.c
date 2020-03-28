c16e399568638837a59918b4e24c5b2391f1772a
Mushfik <ghostmushfik@gmail.com>
2020-03-27 22:35:49 -0600

    Second commit

--- TicTacToe.c
+++ TicTacToe.c
@@ -30,63 +30,61 @@ void printBoard(){
 }
 
 int playerInput() {
-    char c[2]; int p = 0;
+    char c[2]; int p = 0, s;
     printf("Player Move\nEnter an integer between 1-9:\n");
     while (p < 1 || p > 9){
         gets(c);
-        int p = atoi(c);
+        p = atoi(c);
         if(p > 0 && p < 10) {
-            if(spaceAvailable(p) == 0) {
+	    s = spaceAvailable(p);
+            if(s == 0) {
                 p = 0;
                 printf("The space is filled. Choose another space.\nEnter an integer between 1-9:\n");
             }
-        }
-        else printf("Invalid Input. Enter an integer between 1-9:\n");
+        } else printf("Invalid Input. Enter an integer between 1-9:\n");
     }
-    return atoi(c);
+    return p;
 }
 
 int aiInput(){
 
 }
 
-int spaceAvailable(int p) {
+int spaceAvailable(int p){
     if(board[(p-1)/3][(p-1)%3] == '#') return 1;
-    printf("reached");
     return 0;
 }
 
-void updateBoard(int playerID, int p) {
+void updateBoard(int playerID, int p){
     board[(p-1)/3][(p-1)%3] = (playerID) ? 'O' : 'X';
 }
 
 int checkFinsihed() {
-    int i, j, lines[8] = {0}, filled = 1, isWon = 0;
+    int i, j, lines[8]={0}, filled = 1;
     char c;
-    memset(lines, 1, sizeof(lines));
     for(i = 0; i < 3; i++) {
         for(j = 0; j < 3; j++){
-            if(board[i][0] != board[i][j]) lines[i] = 0;
-            if(board[0][i] != board[j][i]) lines[3+i] = 0;
+            if(board[i][0] != board[i][j]) lines[i] = 1;
+            if(board[0][i] != board[j][i]) lines[i+3] = 1;
             if(board[i][j] == '#') filled = 0;
         }
-        if(board[0][0] != board[i][i]) lines[6] = 0;
-        if(board[2][0] != board[2-i][i]) lines[7] = 0;
+        if(board[0][0] != board[i][i]) lines[6] = 1;
+        if(board[2][0] != board[2-i][i]) lines[7] = 1;
     }
 
     for(i = 0; i < 8; i++) {
-        if(lines[i] == 1) {
+        if(lines[i] == 0) {
             if(i < 3) c = board[i][0];
-            else if (i < 6) c = board[0][i];
-            else c = board[i][i];
+            else if (i < 6) c = board[0][i-3];
+            else c = board[1][1];
             if(c != '#'){
-                validateWin(i);
+                validateWin(c);
                 return 1;
             }
         }
     }
 
-    if(filled) printf("\nIt appears that the Champion is stil unbeaten. Better luck next time.");
+    if(filled == 1) printf("\nIt appears that the Champion is stil unbeaten. Better luck next time.\n");
 
     return filled;
 }
@@ -97,7 +95,7 @@ void validateWin(char c) {
 }
 
 void aiWins(){
-    printf("\nYou are not yet worthy to join the CoolKidzClub.");
+    printf("\nYou are not yet worthy to join the CoolKidzClub.\n");
 }
 
 void playerWins(){
@@ -111,18 +109,18 @@ void playerWins(){
     while (fgets(line, sizeof(line), fp)){
         len = strlen(line);
         if(line[len-1] == '\n') line[len-1] = 0;
-        if(strcmp(name, line) != 0) {
-            printf("You are already in the CoolKidzClub.");
+        if(strcmp(name, line) == 0) {
+            printf("You are already in the CoolKidzClub.\n");
             alreadyIn = 1;
             break;
         }
     }
     if(!alreadyIn) fprintf(fp, "%s", name);
-    printf("Done.");
+    printf("Done.\n");
     fclose(fp);
 }
 
-const char * getUserName() {
+const char * getUserName(){
     uid_t uid = geteuid();  
     struct passwd *pw = getpwuid(uid);
     if (pw) return pw->pw_name;
@@ -130,10 +128,10 @@ const char * getUserName() {
 }
 
 int main() {
-    short isFinished = 0, playerID = 0;
+    int isFinished = 0, playerID = 0;
     printf("Can you beat the unbeatable TicTacToe Champion and join the \"CoolKidzClub\"?\n\nPlayers going first have a higher chance of winning. So, make your move.\n");
     printBoard();
-    while (!isFinished){
+    while (isFinished==0){	
         updateBoard(playerID, (playerID) ? playerInput() : playerInput());
         printBoard();
         playerID = (playerID ^ 1) & 1;
@@ -141,3 +139,4 @@ int main() {
     }
     return 0;
 }
+
